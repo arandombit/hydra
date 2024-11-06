63fc6c241bd61c819efc1d6d2d38be7991564498
arandombit <a.random.luis@proton.me>
2024-11-06 14:56:27 -0500

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import { invoke } from '@tauri-apps/api/tauri'
+
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      Hello, world!
+    </div>
+  )
+}
+
+export default App
