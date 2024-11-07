2e82051e127dc223c8879ee372121ba7c2f892fb
arandombit <a.random.luis@proton.me>
2024-11-06 20:32:45 -0500

    integrate react-query and radix-ui

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { Theme } from '@radix-ui/themes'
+
+import '@radix-ui/themes/styles.css'
 
 import App from './App'
 
+const client = new QueryClient()
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider {...{ client }}>
+      <Theme appearance='dark' accentColor='gray' grayColor='slate' radius='small'>
+        <App />
+      </Theme>
+    </QueryClientProvider>
   </React.StrictMode>
 )
